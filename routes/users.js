0d261b6d2947c827e47318c13bf97f3a1d8196f6
Miguel Tornero <a00820449@itesm.mx>
2022-10-19 02:24:36 -0500

    inital functionality

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 const Router = require("koa-router")
+const koaBody = require('koa-body')
 
 const router = new Router()
 
-router.get("/create", (ctx, next)=>{
+router.get("/auth", (ctx, next)=>{
     ctx.body = ctx.request.query
+})
+
+router.post("/create", (ctx, next)=>{
+    ctx.status = 200
+    ctx.body = ctx.request.body
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const Koa = require("koa")
+const koaBody = require('koa-body')
+const cors = require("kcors")
 const Router = require("koa-router")
 const userRouter = require("./routes/users")
 
 const app = new Koa()
 const router = new Router()
 
-router.use("/users", userRouter.routes(), userRouter.allowedMethods())
+router.use("/users", userRouter.routes())
+router.use("/users", userRouter.allowedMethods())
+
+app.use(koaBody())
+app.use(cors())
 app.use(router.routes())
+app.use(router.allowedMethods())
 
 module.exports = app
