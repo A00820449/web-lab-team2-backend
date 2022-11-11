c476a333137c428f5fb5665cfc078602fcc988a6
Miguel Tornero <a00820449@itesm.mx>
2022-11-11 15:26:59 -0600

    added morgan logging

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const koaBody = require('koa-body')
 const cors = require("kcors")
 const Router = require("koa-router")
 const userRouter = require("./routes/users")
+const morgan = require('koa-morgan')
 
 const app = new Koa()
 const router = new Router()
@@ -14,5 +15,6 @@ app.use(koaBody())
 app.use(cors())
 app.use(router.routes())
 app.use(router.allowedMethods())
+app.use(morgan("dev"))
 
 module.exports = app
