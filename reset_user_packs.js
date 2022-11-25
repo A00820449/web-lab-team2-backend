bfb0509f205311e68c1ce66bdf00fe55774a6d64
Miguel Tornero <a00820449@itesm.mx>
2022-11-25 16:35:21 -0600

    updated script

--- reset_user_packs.js
+++ reset_user_packs.js
@@ -1,9 +1,12 @@
 require("dotenv").config()
 const { User, close, connect } = require("./db")
+const { writeFile } = require("node:fs/promises")
+const path = require("node:path")
 
 async function main() {
     await connect()
     await User.updateMany({},{lastFreePack: -1})
+    await writeFile(path.resolve(__dirname, "routes", "_pack"), JSON.stringify(0))
 }
 
 main().catch(console.error).finally(()=>close().catch(console.error))
